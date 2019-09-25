ba048a6e070ff3e41da9a38649a11e5cfa69ab0d
bergpb <lindembergdepaulo@gmail.com>
2019-09-25 15:45:29 -0300

    aula11: editando dados

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
-    path('task/<int:id>', views.taskView, name='task-view'),
     path('newtask/', views.newTask, name='new-task'),
+    path('task/<int:id>', views.taskView, name='task-edit'),
+    path('edit/<int:id>', views.editTask, name='task-view'),
     path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
