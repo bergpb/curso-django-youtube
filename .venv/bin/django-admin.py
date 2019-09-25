de4a9f9f13d3570b011c54f7a99c0fbb2efa5bb2
bergpb <lindembergdepaulo@gmail.com>
2019-09-25 14:20:28 -0300

    aula9: inserindo dados no banco

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/art3mix/working_on/cursodjango2/.venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .forms import TaskForm
 
 from .models import Task
 
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/new.html', {'form': form})
+
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/lists.html', {'tasks': tasks})
 
 def taskView(request, id):
