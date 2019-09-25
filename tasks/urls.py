45c3d9324515d64b2447165eea43e12cee5fdd39
bergpb <lindembergdepaulo@gmail.com>
2019-09-25 15:59:02 -0300

    aula12: removendo do banco

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
     path('newtask/', views.newTask, name='new-task'),
-    path('task/<int:id>', views.taskView, name='task-edit'),
+    path('task/<int:id>', views.taskView, name='edit-task'),
     path('edit/<int:id>', views.editTask, name='task-view'),
+    path('delete/<int:id>', views.deleteTask, name='delete-task'),
     path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
