edf99b4a2a936f474a2a9866b29758851e21c5f1
bergpb <lindembergdepaulo@gmail.com>
2019-09-25 21:35:04 -0300

    aula15: login e logout

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
     path('newtask/', views.newTask, name='new-task'),
     path('task/<int:id>', views.taskView, name='edit-task'),
     path('edit/<int:id>', views.editTask, name='task-view'),
     path('delete/<int:id>', views.deleteTask, name='delete-task'),
-    path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
@@ -6,6 +7,7 @@ from django.contrib import messages
 
 from .models import Task
 
+@login_required
 def newTask(request):
     if(request.method == 'POST'):
         form = TaskForm(request.POST)
@@ -20,6 +22,7 @@ def newTask(request):
         form = TaskForm()
         return render(request, 'tasks/new.html', {'form': form})
 
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -35,6 +38,7 @@ def editTask(request, id):
     else:
         return render(request, 'tasks/edit.html', {'form': form, 'task': task})
 
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
@@ -43,6 +47,7 @@ def deleteTask(request, id):
 
     return redirect('/')
 
+@login_required
 def taskList(request):
     search = request.GET.get('search')
 
@@ -61,12 +66,7 @@ def taskList(request):
 
     return render(request, 'tasks/lists.html', {'tasks': tasks})
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
-
-def helloWorld(request):
-    return HttpResponse('Hello word!')
-
-def yourName(request, name):
-    return render(request, 'tasks/yourname.html', {'name': name})
